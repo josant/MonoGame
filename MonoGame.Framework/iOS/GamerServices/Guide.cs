b4a5ee463255f6cf327930436f356987f8764d96
Joshua Anthony <joshua.anthony@habanero.se>
2013-07-11 13:12:48 +0200

    Updates for Xamarin over Novell.

--- MonoGame.Framework/iOS/GamerServices/Guide.cs
+++ MonoGame.Framework/iOS/GamerServices/Guide.cs
@@ -64,6 +64,9 @@ change. To the extent permitted under your local laws, the contributors exclude
 the implied warranties of merchantability, fitness for a particular purpose and
 non-infringement.
 */
+using System.Text;
+
+
 #endregion License
 
 using System;
